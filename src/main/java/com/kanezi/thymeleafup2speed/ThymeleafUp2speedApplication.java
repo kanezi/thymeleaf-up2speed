4a927489e927e64fc2994053e751b43b9438baab
kanezi <kanezi.com@gmail.com>
2023-02-22 20:55:38 +0100

    project scaffold

---
+++ src/main/java/com/kanezi/thymeleafup2speed/ThymeleafUp2speedApplication.java
@@ -0,0 +1,13 @@
+package com.kanezi.thymeleafup2speed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafUp2speedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafUp2speedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanezi/thymeleafup2speed/ThymeleafUp2speedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanezi.thymeleafup2speed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafUp2speedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
